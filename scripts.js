747b198da652a627329e737c428a7df69dcd131f
graf-jordan <jordangraf42@gmail.com>
2023-11-24 10:52:04 +1030

    Return color of squares to neutral

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 const cont = document.querySelector(".squareContainer");
-console.log("Hello");
 
 for (let i = 0; i < 256; i++) {
     let etchElement = document.createElement("div");
-    etchElement.className = "etchSquare";
-    etchElement.style.width = "2px";
-    etchElement.style.height = "2px";
-    etchElement.style.backgroundColor = "black";
+    etchElement.className = "etchElement";
+    etchElement.style.width = "6%";
+    etchElement.style.height = "6%";
+
     etchElement.addEventListener("mouseover", () => {
         etchElement.style.backgroundColor = "red";
     });
