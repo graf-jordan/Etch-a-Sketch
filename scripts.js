7f4814704143dab0cd96e3c72f2fea38772fda7f
graf-jordan <jordangraf42@gmail.com>
2023-11-24 11:18:30 +1030

    change values of squares

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 const cont = document.querySelector(".squareContainer");
+console.log("Hello");
 
 for (let i = 0; i < 256; i++) {
     let etchElement = document.createElement("div");
     etchElement.className = "etchElement";
-    etchElement.style.width = "6%";
-    etchElement.style.height = "6%";
+    etchElement.style.width = "62.5px";
+    etchElement.style.height = "62.5px";
 
     etchElement.addEventListener("mouseover", () => {
-        etchElement.style.backgroundColor = "red";
+        etchElement.style.backgroundColor = "gray";
     });
 
     cont.appendChild(etchElement);
