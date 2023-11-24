453e4bff1b8a81bafd5c7d6cf061a39117005af4
graf-jordan <jordangraf42@gmail.com>
2023-11-24 12:23:13 +1030

    Clean up JS and place everything in functions

--- scripts.js
+++ scripts.js
@@ -1,24 +1,39 @@
 const cont = document.querySelector(".squareContainer");
+let gridSize;
+let gridDimensions;
 
+//placeholder to get size for number of pixel boxes
 function getSize() {
     const gridSize = window.prompt("What size grid would you like?");
     return gridSize;
 }
 
-const gridDimensions = getSize();
-const gridSize = 1000/gridDimensions;
-console.log(gridSize);
-console.log(gridDimensions);
-
-for (let i = 0; i < (gridDimensions*gridDimensions); i++) {
-    let etchElement = document.createElement("div");
-    etchElement.className = "etchElement";
-    etchElement.style.width = `${gridSize}px`;
-    etchElement.style.height = `${gridSize}px`;
-
-    etchElement.addEventListener("mouseover", () => {
-        etchElement.style.backgroundColor = "gray";
-    });
-
-    cont.appendChild(etchElement);
+//parse grid dimensions
+function chooseDimensions() {
+    gridDimensions = getSize();
+    gridSize = 1000/gridDimensions;
+    console.log(gridSize);
+    console.log(gridDimensions);
 }
+
+//create dimensions of grid
+function makeGrid() {
+    for (let i = 0; i < (gridDimensions*gridDimensions); i++) {
+        let etchElement = document.createElement("div");
+        etchElement.className = "etchElement";
+        etchElement.style.width = `${gridSize}px`;
+        etchElement.style.height = `${gridSize}px`;
+    
+        etchElement.addEventListener("mouseover", () => {
+            etchElement.style.backgroundColor = "gray";
+        });
+        cont.appendChild(etchElement);
+    }
+}
+
+//create grid
+chooseDimensions();
+makeGrid();
+
+
+
